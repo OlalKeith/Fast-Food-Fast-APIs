5aafeaa7e7d3f867e6582ee3562f8bc59e139fe7
Olal Keith <jkeneth.jk@gmail.com>
2018-09-11 12:24:14 +0300

    ignore file

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-import os
 from flask import Flask, jsonify, abort, make_response,  request
+import os
 
 app = Flask(__name__)
 
@@ -103,9 +103,6 @@ def update_order(order_id):
 
 
     }
-
-
-
 if __name__ == '__main__':
     # app.run(debug=True)
     port = int(os.environ.get('PORT', 5000))
