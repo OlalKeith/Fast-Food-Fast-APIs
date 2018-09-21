bcde87499e3d5690e9a90ecd2ab3d59cde652011
Olal Keith <jkeneth.jk@gmail.com>
2018-09-21 11:33:58 +0300

    testing place order

--- app/tests/v1/test_views_orders.py
+++ app/tests/v1/test_views_orders.py
@@ -18,3 +18,7 @@ class TestGettingAllorders(unittest.TestCase):
 	def test_single_order(self):
 		make_response = self.client.get('/api/v1/orders/<int:order_id>', content_type='application/json')
 		self.assertEqual(make_response.status_code, 404)
+
+	def test_place_oder(self):
+		make_response = self.client.post('/api/v1/order', content_type='application/json' )
+		self.assertEqual(make_response.status_code, 400)
